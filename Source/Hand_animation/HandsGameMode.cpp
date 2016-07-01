3fc0f3a3580e45d90254916f9a3f36922b99c30a
Nestor Lobato <s1572614@sms.ed.ac.uk>
2016-07-01 21:17:45 +0100

    Added a basic UE4 GameMode functionality template to the project

--- Source/Hand_animation/HandsGameMode.cpp
+++ Source/Hand_animation/HandsGameMode.cpp
@@ -2,7 +2,75 @@
 
 #include "Hand_animation.h"
 #include "HandsGameMode.h"
+#include "Hands_Character.h"
+#include "Kismet/GameplayStatics.h"
 
+AHandsGameMode::AHandsGameMode()
+{
 
+}
 
+void AHandsGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AHandsGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	/*AHands_Character* MyCharacter = Cast<AHands_Character>(UGameplayStatics::GetPlayerPawn(this, 0));
+	if (MyCharacter)
+	{
+
+	}*/
+}
+
+void AHandsGameMode::SetCurrentState(EExperimentPlayState NewState)
+{
+	// set current state
+	CurrentState = NewState;
+	// handle current state
+	HandleNewState(CurrentState);
+}
+
+EExperimentPlayState AHandsGameMode::GetCurrentState() const
+{
+	return CurrentState;
+}
+
+void AHandsGameMode::HandleNewState(EExperimentPlayState NewState)
+{
+	switch (NewState)
+	{
+	case EExperimentPlayState::ECalibration:
+		{
+	}
+		break;
+		
+	case EExperimentPlayState::ESynchronous:
+	{
+
+	}
+		break;
+
+	case EExperimentPlayState::EAsynchronous:
+	{
+
+	}
+		break;
+
+	case EExperimentPlayState::EFinished:
+	{
+
+	}
+		break;
+
+	case EExperimentPlayState::EUnknown:
+	{
+
+	}
+		break;
+	}
+}
 
--- Source/Hand_animation/HandsGameMode.h
+++ Source/Hand_animation/HandsGameMode.h
@@ -1,23 +1,18 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "GameFramework/GameMode.h"
 #include "HandsGameMode.generated.h"
 
-/**
- * 
- */
-
 // Enum to store the current state of gameplay
-//UENUM(BlueprintType)
-//enum class EBatteryPlayState
-//{
-//	EPlaying,
-//	EGameOver,
-//	EWon,
-//	EUnknown
-//};
+UENUM(BlueprintType)
+enum class EExperimentPlayState
+{
+	ECalibration,
+	ESynchronous,
+	EAsynchronous,
+	EFinished,
+	EUnknown
+};
 
 UCLASS()
 class HAND_ANIMATION_API AHandsGameMode : public AGameMode
@@ -26,12 +21,24 @@ class HAND_ANIMATION_API AHandsGameMode : public AGameMode
 	
 public:
 
-	///** Returns the current playing state */
-	//UFUNCTION(BlueprintPure, Category = "Power")
-	//EBatteryPlayState GetCurrentState() const;
+	AHandsGameMode();
+	
+	virtual void BeginPlay() override;
 
-	///** Sets a new playing state */
-	//void SetCurrentState(EBatteryPlayState NewState);
+	virtual void Tick(float DeltaTime) override;
 	
-	
+	/** Returns the current playing state */
+	UFUNCTION(BlueprintPure, Category = "Power")
+	EExperimentPlayState GetCurrentState() const;
+
+	/** Sets a new playing state */
+	void SetCurrentState(EExperimentPlayState NewState);
+
+private:
+	/** Keeps track of the current playing state */
+	EExperimentPlayState CurrentState;
+
+	/** handle any functions calls that rely upon changing the playing state of our game */
+	void HandleNewState(EExperimentPlayState NewState);
+
 };
